91d7b7cb934b92194edf5a6b20be795d9395d4fe
Kan - Singh <kanishka.singh416@gmail.com>
2024-11-10 16:05:10 -0800

    week3_commit2_navbarOnPages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
 export default function Home() {
-  return <Navbar />;
-}
-
-function Navbar() {
   return (
     <nav className="border-b sticky top-0 bg-gray-900 text-gray-100 border-gray-800 z-10">
       <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
